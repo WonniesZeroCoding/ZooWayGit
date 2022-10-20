b070c459f8110af3058ded8c27eef618e81b7741
HaMsCodE <gkarudtlr@nate.com>
2022-10-20 21:03:08 +0900

    hks - orderMain - 2022 10 20

--- src/view/OrderMain.java
+++ src/view/OrderMain.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.util.Calendar;
 
 import dao.OrderDAO;
+import dao.VisitDAO;
 import dto.MemberDTO;
 import dto.OrderDTO;
 import functions.calendar;
@@ -64,6 +65,7 @@ public class OrderMain {
 	    	System.out.println("|           선택하신 날짜는 "+arr[0]+"년"+arr[1]+"월 "+arr[2]+"일입니다 맞습니까?             ");
 			System.out.println("|           1.확인  2.다른 날짜 선택  3.메인페이지로                 ");
 			System.out.println(" -----------------------------------------------");
+			System.out.print(">>");
 			int num3 = Integer.parseInt(br.readLine());
 	    	if(num3==1) {
 	    		visitDate = arr[0]+"-"+arr[1]+"-"+arr[2];
@@ -79,6 +81,8 @@ public class OrderMain {
 	    	System.out.println("|           선택하신 시간번호는 "+timeNum+"입니다 맞습니까?");
 			System.out.println("|           1.확인  2.다른 시간 선택  3.메인페이지로     ");
 			System.out.println(" -----------------------------------------------");
+			System.out.print(">>");
+
 	    	int num4 = Integer.parseInt(br.readLine());
 	    	if(num4==1) {//확인을 눌렀을시
 	    		break;
@@ -89,9 +93,7 @@ public class OrderMain {
 	    	}
 	    	int[] arr2 = new int[2]; 
 	    	arr2 = new OrderDAO().polSelect();  //policy 받기
-	    	
-//System.out.println("최종 insert 전 확인");
-	    	
+
 	    	int mnum = member.getMnum();
 //	    	System.out.println(visitDate);//방문날짜
 //	    	System.out.println(timeNum);//시간번호
@@ -100,18 +102,19 @@ public class OrderMain {
 //	    	System.out.println(arr2[1]);//개월당 금액
 //	    	System.out.println(homeNum); //자가 or 방문
 	    	
-	    	new OrderDAO().productMinus(num); //재고 -
+	    	new OrderDAO().productMinus(num); //재고 마이너스
 	    	
-	    	OrderDTO order = new OrderDTO(visitDate,arr2[0]);
+	    	OrderDTO order = new OrderDTO(visitDate,arr2[0]); //visitDate와 개월수 저장
 	    	
-	    	String endDate = new functions.calendar().CompareDate(order);
+	    	String endDate = new functions.calendar().CompareDate(order); //endDate 받아오기
 	    	
 	    	new OrderDAO().insertOrder(mnum,visitDate,timeNum,arr2[0],num,arr2[1],homeNum,endDate); //주문테이블에 추가
 	    	
 	    	new OrderDAO().showResult(mnum, num); // 신청확인내역 띄워주기
 	    		    	
-	    	
-	    	
+	    	int onum = new OrderDAO().selectOrderNum(member.getMnum());//가장 최신 onum 가져오기
+	    	 
+	    	new VisitDAO().insertVisit(mnum,onum,visitDate,timeNum);//VISIT 테이블에추가 
 	    	
 	    	///이후에 이제.... 그... 기사한테 메세지 보내는 거?? ㅋ
 	    
